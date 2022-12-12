8dcd1790fcb0fb70ca0e0614101d8a9473c15a22
Yevgenii <yevgenii@test123.com>
2022-12-12 13:34:31 +0200

    add Dockerfile and package.json

--- app/nodeapp.js
+++ app/nodeapp.js
@@ -10,18 +10,18 @@ app.use(express.urlencoded({ extended: false }));
 // Connect to MongoDB
 mongoose
   .connect(
-    'mongodb://mongo:27017/docker-node-mongo',
+    'mongodb://mongodb:27017/docker-node-mongo',
     { useNewUrlParser: true }
   )
   .then(() => console.log('MongoDB Connected'))
   .catch(err => console.log(err));
 
-const Item = require('./models/Item');
+const Item = require('./models/item');
 
 app.get('/', (req, res) => {
   Item.find()
     .then(items => res.render('index', { items }))
-    .catch(err => res.status(404).json({ msg: 'No items found' }));
+    .catch(err => res.status(404).json({ msg: 'No items found!' }));
 });
 
 app.post('/item/add', (req, res) => {
