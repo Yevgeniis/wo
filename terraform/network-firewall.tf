c793f2d98de4e816a863ee045f19a031ea46512f
Yevgenii <yevgenii@test123.com>
2022-12-16 18:17:50 +0200

    add http rule

--- terraform/network-firewall.tf
+++ terraform/network-firewall.tf
@@ -22,3 +22,16 @@ resource "google_compute_firewall" "allow-ssh" {
     source_ranges = ["0.0.0.0/0"] 
     target_tags = ["ssh"]
 }
+
+
+# allow http
+resource "google_compute_firewall" "allow-http" {
+    name = "wo-app-allow-http"
+    network = google_compute_network.wo-app-vpc.name
+    allow {
+        protocol = "tcp"
+        ports = ["80"]       
+    } 
+    source_ranges = ["0.0.0.0/0"] 
+    target_tags = ["http"]
+}
