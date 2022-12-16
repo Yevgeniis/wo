52b91ff4c47ba84b461f08af98389049d961d4b6
Yevgenii <yevgenii@test123.com>
2022-12-16 16:00:56 +0200

    add network-firewall rules

---
+++ terraform/network-firewall.tf
@@ -0,0 +1,24 @@
+# allow ICMP Protocol
+
+resource "google_compute_firewall" "allow-icmp" {
+
+    name = "wo-app-allow-icmp"
+    network = google_compute_network.wo-app-vpc.name
+    allow {
+      protocol = "icmp"
+    }
+    source_ranges = ["0.0.0.0/0"]
+    target_tags = ["icmp"]
+}
+
+# allow ssh
+resource "google_compute_firewall" "allow-ssh" {
+    name = "wo-app-allow-ssh"
+    network = google_compute_network.wo-app-vpc.name
+    allow {
+        protocol = "tcp"
+        ports = ["22"]       
+    } 
+    source_ranges = ["0.0.0.0/0"] 
+    target_tags = ["ssh"]
+}
